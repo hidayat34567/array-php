425786bc47fcbf5317232cbf008b80ee72305bd1
hidayat34567 <2223622.wafa@smkn-2sbg.sch.id>
2024-02-12 07:05:38 +0700

    data-type

---
+++ data type_1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+var_dump($x);
+?>
+
+</body>
+</html>
+
+
+<"DATA DIRI"</h1>
+    <table width="500" border="1" cellspasing="0" cellspasing="5" align="center">
+        <tr align="center" bgcolor="blue">
+            <td width="170">DATA DIRI</td><header>
+    <h1 align="ceter
+            <td width="350">KETERANGAN</td>
+            <td width="200">FOTO</td>
+        </tr>
+        <tr>
+            <td>NAMA</td>
+            <td>Wafa Habibah Hidayat</td>
+            <td rowspan="10" align="center"><img src="habibah.jpg" width="200" height="250"></td>
+        </tr>
+        <tr>
+            <td>Jurusan</td>
+            <td>RPL</td>
+        </tr>
+        <tr>
+            <td>Sekolah</td>
+            <td>SMKN 2 SUBANG</td>
+        </tr>
+        <tr>
+            <td>Tanggal</td>
+            <td>7-februari-2024</td>
+        </tr>
+        </table> 
---
+++ data type_2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Hello world!";
+$y = 'Hello world!';
+
+var_dump($x);
+echo "<br>"; 
+var_dump($y);
+?>
+
+</body>
+</html>
+
+<"DATA DIRI"</h1>
+    <table width="500" border="1" cellspasing="0" cellspasing="5" align="center">
+        <tr align="center" bgcolor="blue">
+            <td width="170">DATA DIRI</td><header>
+    <h1 align="ceter
+            <td width="350">KETERANGAN</td>
+            <td width="200">FOTO</td>
+        </tr>
+        <tr>
+            <td>NAMA</td>
+            <td>Wafa Habibah Hidayat</td>
+            <td rowspan="10" align="center"><img src="habibah.jpg" width="200" height="250"></td>
+        </tr>
+        <tr>
+            <td>Jurusan</td>
+            <td>RPL</td>
+        </tr>
+        <tr>
+            <td>Sekolah</td>
+            <td>SMKN 2 SUBANG</td>
+        </tr>
+        <tr>
+            <td>Tanggal</td>
+            <td>18-oktober-2023</td>
+        </tr>
+        </table> 
